d9c8ceb7346746c49f6e0dc5599440dd271d8f17
Viniciusdossantosandrade <162480502+Viniciusdossantosandrade@users.noreply.github.com>
2025-02-13 18:24:11 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2f, sou o vini </p>
+
+    <?php
+    $nome = "vini";
+    echo "<p> Oi 2f, sou o vini</p>";
+    echo "<p>Oi 2f, sou o $nome </p>";
+    echo "<p>Oi 2f , sou o ".$nome." </p>";
+    
+    ?>
+</body>
+</html>
