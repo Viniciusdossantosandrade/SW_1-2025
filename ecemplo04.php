1b2f5d0d57f63f0f766f99f1ed43e38a6e5e77d6
Viniciusdossantosandrade <162480502+Viniciusdossantosandrade@users.noreply.github.com>
2025-02-17 14:59:56 -0300

    Add files via upload

---
+++ ecemplo04.php
@@ -0,0 +1,10 @@
+<?php
+   define("vinicius",1.5);
+   $xyz = 1.5;
+
+
+   echo " O valor é" .vinicius;
+   echo "<br>";
+   echo "O valor é $xyz";
+   
+?>
---
+++ exemplo06.php
@@ -0,0 +1,15 @@
+<?php
+   $nome = "vinicius";
+   $idade = 12;
+   $funcao = "Aluno";
+
+   $frase = "Dados pessoais";
+   $frase = $nome;
+   $frase = " - ";
+   $frase = $idade;
+   $frase = " - ";
+   $frase = $funcao;
+
+   echo $frase;
+?>
+
