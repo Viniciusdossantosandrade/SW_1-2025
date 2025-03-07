39eb3f1c91d095257ebce0aca87c4b2e8a4c6285
Viniciusdossantosandrade <162480502+Viniciusdossantosandrade@users.noreply.github.com>
2025-03-07 16:56:27 -0300

    Add files via upload

---
+++ aula04/exemplo01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+ <form action="" method="get"></form>
+
+<h1>Formulários</h1>
+
+<form action="/action_page.php">
+  <label for="fname">Nome:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Nota1:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="lname">Nota2:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="lname">Nota3:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Calcular média">
+  
+</form>
+
+
+</body>
+</html>
---
+++ aula04/form01.php
@@ -0,0 +1,4 @@
+<?php
+$nota_media = $_GET ($nota1, $nota2 , $nota3 );
+
+?>
