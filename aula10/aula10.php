bb95a1cff0121a9515430ab13737c8feb77f2b2a
Viniciusdossantosandrade <162480502+Viniciusdossantosandrade@users.noreply.github.com>
2025-05-16 18:32:04 -0300

    Add files via upload

---
+++ aula10/aula10.php
@@ -0,0 +1,24 @@
+<?php
+    include_once 'Conta.class.php';
+
+    $conta1 = new Conta();
+    //var_dump($conta1);
+
+    $conta1->Nome = ":Vinicius";
+    $conta1->Cpf = ":123.456.789-00";
+    //$conta1->Saldo = 500;
+
+    $conta1->ConsultarSaldo();
+    echo"<hr>";
+
+    echo $conta1->Depositar(250);
+    echo "<hr>";
+
+    echo $conta1->Sacar(800);
+    echo "<hr>";
+
+
+
+
+    
+?>
